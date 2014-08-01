f9380fc41039d80ca94307e5e8104d6ea5fae819
Kurt Harriger <kurtharriger@gmail.com>
2014-08-01 08:56:09 -0600

    verify path option present before resolving

--- tasks/webpack-dev-server.js
+++ tasks/webpack-dev-server.js
@@ -50,7 +50,9 @@ module.exports = function(grunt) {
 			options.contentBase = path.resolve(process.cwd(), options.contentBase);
 		[].concat(options.webpack).forEach(function(webpackOptions) {
 			webpackOptions.context = path.resolve(process.cwd(), webpackOptions.context);
-			webpackOptions.output.path = path.resolve(process.cwd(), webpackOptions.output.path);
+			if(webpackOptions.output && webpackOptions.output.path) {
+				webpackOptions.output.path = path.resolve(process.cwd(), webpackOptions.output.path);
+			}
 		});
 
 		console.log(options);
