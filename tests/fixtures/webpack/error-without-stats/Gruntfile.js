43b380e4d9d458c097cb5519f6b7b25e5ce2ff9a
Daniel Tschinder <code@tschinder.de>
2016-12-10 20:45:35 +0100

    Add test for error without stats

---
+++ tests/fixtures/webpack/error-without-stats/Gruntfile.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+const loadGruntWebpackTasks = require('../../../utils/loadGruntWebpackTasks');
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    webpack: {
+      test: {
+        stats: false,
+        entry: path.join(__dirname, "entry"),
+        output: {
+          path: __dirname,
+          filename: "output.js",
+        }
+      },
+    },
+  });
+
+  loadGruntWebpackTasks(grunt);
+};
---
+++ tests/fixtures/webpack/error-without-stats/entry.js
@@ -0,0 +1,2 @@
+var a = require('./b');
+console.log(a);
