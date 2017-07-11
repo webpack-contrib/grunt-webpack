d01aba9b45f1f3173dc99134b158cc6e5b84d724
Daniel Tschinder <daniel@tschinder.de>
2017-07-11 15:04:01 +0200

    Ensure that the cache default functions gets correctly executed (#150)

--- tests/src/options/WebpackOptionHelper.js
+++ tests/src/options/WebpackOptionHelper.js
@@ -1,13 +1,31 @@
 import test from 'ava';
 import WebpackOptionHelper from '../../../src/options/WebpackOptionHelper';
 
-test('cache is enabled in watch mode', (t) => {
+test('cache stays enabled in watch mode', (t) => {
   const options = { watch: true, cache: true };
   const helper = new WebpackOptionHelper();
 
   const result = helper.filterOptions(options);
 
   t.true(result.cache);
+});
+
+test('cache is enabled in watch mode by default', (t) => {
+  const options = { watch: true, cache: () => true };
+  const helper = new WebpackOptionHelper();
+
+  const result = helper.filterOptions(options);
+
+  t.true(result.cache);
+});
+
+test('cache is disabled in normal mode by default', (t) => {
+  const options = { watch: false, cache: () => true };
+  const helper = new WebpackOptionHelper();
+
+  const result = helper.filterOptions(options);
+
+  t.false(result.cache);
 });
 
 test('cache is disabled in normal mode', (t) => {
